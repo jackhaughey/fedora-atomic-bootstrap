b954eb529bc4abdc95edabb9971b717040f2e53c
Jack Haughey <jack.haughey@gmail.com>
2026-04-29 16:39:28 +0100

    adding flatpak install

---
+++ install-flatpaks.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Enabling Flathub..."
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo "Installing Firefox..."
+flatpak install -y flathub org.mozilla.firefox
+
+echo "Installing Visual Studio Code..."
+flatpak install -y flathub com.visualstudio.code
+
+echo "Installing Shotwell..."
+flatpak install -y flathub org.gnome.Shotwell
+
+echo "Installing VLC..."
+flatpak install -y flathub org.videolan.VLC
+
+echo "Installing Obsidian..."
+flatpak install -y flathub md.obsidian.Obsidian
+
+echo "All Flatpak applications installed successfully."
+
