8a66a7452c44657e450635d276fac364f3ce4ec1
Jack Haughey <jack.haughey@gmail.com>
2026-04-29 16:33:43 +0100

    change to apps in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Installing layered packages..."
-sudo rpm-ostree install $(cat packages.txt)
+echo "Removing default Firefox RPM..."
+sudo rpm-ostree override remove firefox
 
-echo "Rebooting to apply layers..."
+echo "Installing layered packages..."
+sudo rpm-ostree install \
+  git \
+  openssh-clients \
+  tmux \
+  htop \
+  alacritty \
+  chezmoi \
+  distrobox
+
+echo "Rebooting to apply layered changes..."
 systemctl reboot
