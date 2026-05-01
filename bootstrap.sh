28ef4e39f12147ca336b8502fb156a9c26702aa1
Jack Haughey <jack.haughey@gmail.com>
2026-05-01 11:59:14 +0100

    bootstrap update

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,17 +14,8 @@ sudo rpm-ostree install \
     distrobox
 
 echo "==> Installing Starship prompt"
-STARSHIP_VERSION="1.18.2"
-STARSHIP_URL="https://github.com/starship/starship/releases/download/v${STARSHIP_VERSION}/starship-x86_64-unknown-linux-gnu.tar.gz"
-
-mkdir -p /tmp/starship-install
-curl -fsSL "${STARSHIP_URL}" -o /tmp/starship-install/starship.tar.gz
-tar -xzf /tmp/starship-install/starship.tar.gz -C /tmp/starship-install
-
-install -Dm755 /tmp/starship-install/starship /usr/local/bin/starship
-
-echo "==> Cleaning up Starship temp files"
-rm -rf /tmp/starship-install
+curl -sS https://starship.rs/install.sh | sh -s -- -b ~/.local/bin
+starship preset catppuccin-powerline -o ~/.config/starship.toml
 
 echo "==> Rebooting to apply rpm-ostree changes"
 systemctl reboot
