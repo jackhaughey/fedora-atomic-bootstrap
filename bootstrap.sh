8d19bff16c29c39f1a7f21abc760ae6e252b0e6e
Jack Haughey <jack.haughey@gmail.com>
2026-05-06 17:18:13 +0100

    removed adding of git. not required

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,6 @@ sudo rpm-ostree override remove firefox firefox-langpacks
 echo "==> Applying rpm-ostree package layering"
 sudo rpm-ostree install \
     zsh \
-    git \
     tmux \
     htop \
     alacritty \
@@ -17,7 +16,7 @@ sudo rpm-ostree install \
 echo "==> Installing Starship prompt"
 mkdir -p "$HOME/.local/bin"
 curl -sS https://starship.rs/install.sh | sh -s -- -b ~/.local/bin
-starship preset catppuccin-powerline -o ~/.config/starship.toml
+# starship preset catppuccin-powerline -o ~/.config/starship.toml
 
 # Starship
 echo eval "$(starship init bash)" >> ~/.bashrc
